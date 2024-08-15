36713e8c6a1da641fff2969de2bed800be08429f
Natã <141784916+333NNN@users.noreply.github.com>
2024-08-15 14:06:50 -0300

    Add files via upload

---
+++ LISTA_10_NATA/src/main/java/com/mycompany/lista_10_nata/LISTA_10_NATA.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista_10_nata;
+
+/**
+ *
+ * @author Nata e Julia
+ */
+public class LISTA_10_NATA {
+
+    public static void main(String[] args) {
+        System.out.println("Contagem regressiva: ");
+        int n = 10;
+        while(n>-1){
+            System.out.print(n+" ");
+            n--;
+        }
+    }
+}
