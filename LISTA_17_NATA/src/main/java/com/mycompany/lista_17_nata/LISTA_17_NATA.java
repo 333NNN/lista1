f7a0fab5c35c583b3a587e9b5a4ce73d5c80d799
Natã <141784916+333NNN@users.noreply.github.com>
2024-08-15 23:31:01 -0300

    Update LISTA_17_NATA.java

--- LISTA_17_NATA/src/main/java/com/mycompany/lista_17_nata/LISTA_17_NATA.java
+++ LISTA_17_NATA/src/main/java/com/mycompany/lista_17_nata/LISTA_17_NATA.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.lista_17_nata;
 
+import java.util.Scanner;
 /**
  *
  * @author Nata e Julia
@@ -11,6 +12,20 @@ package com.mycompany.lista_17_nata;
 public class LISTA_17_NATA {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.print("Digite uma palavra: ");
+        String palavra = ler.nextLine();
+        int vogais = 0;  
+        
+        for (int i = 0; i < palavra.length(); i++) {
+            char letra = palavra.charAt(i);
+            if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
+                letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') {
+                vogais = vogais + 1;
+            }
+            System.out.println(letra);
+        }
+        System.out.println("Existem " + vogais + " vogais na palavra.");
     }
 }
