9b99eaba2f33ea8c2cb1a9623afa225013f36d38
Natã <141784916+333NNN@users.noreply.github.com>
2024-08-15 22:47:50 -0300

    Add files via upload

---
+++ LISTA_17_NATA/src/main/java/com/mycompany/lista_17_nata/LISTA_17_NATA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista_17_nata;
+
+/**
+ *
+ * @author Nata e Julia
+ */
+public class LISTA_17_NATA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
