afae1c4c1c89fb13598f5fec44542b0a68025d8e
Natã Jordão <141784916+333NNN@users.noreply.github.com>
2024-08-16 21:00:27 -0300

    Add files via upload

---
+++ LISTA_20_NATA/src/main/java/com/mycompany/lista_20_nata/LISTA_20_NATA.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista_20_nata;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ncunha
+ */
+public class LISTA_20_NATA {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Bem-vindo(a) à Loja Virtual!\nDigite o valor da compra: ");
+        float valor = ler.nextFloat();
+        boolean valid = true;
+        float valorFinal = 0, porcentCartao = 3.0f/100, porcentVista = 10.0f/100, porcentDebito = 5.0f/100;
+        
+        while (valid){
+            System.out.println("Digite a forma de pagamento:\n1 - Cartão de crédito (sujeito a taxas)\n2 - Dinheiro/Pix\n3 - Cartão débito");
+            int formaPag = ler.nextInt();
+            
+
+            if (formaPag == 1){
+                valorFinal = valor + (porcentCartao * valor);
+                break;
+            }
+            else if (formaPag == 2){
+                valorFinal = valor - (porcentVista * valor);
+                break;
+            }
+            else if (formaPag == 3){
+                valorFinal = valor - (porcentDebito * valor);
+                break;
+            }
+            else {
+                System.out.println("Forma de pagamento inválida.");
+            }
+        }
+        System.out.printf("O valor final da compra é R$%.2f",valorFinal);
+    }
+}
